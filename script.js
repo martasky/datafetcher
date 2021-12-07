56a285e1375a1565cd982145ebe996d50452d6a7
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-12-07 16:23:39 +0100

    add

---
+++ script.js
@@ -0,0 +1,67 @@
+import { endpoint, headers } from "./settings.js";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  let urlBar = "https://foo-bar-project.herokuapp.com/";
+
+  fetch(urlBar)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      prepareData(jsonData);
+    });
+}
+
+function prepareData(jsonData) {
+  let timestamp = jsonData.timestamp;
+  let queue = jsonData.queue.length;
+  let orders = [];
+
+  jsonData.serving.forEach((e) => {
+    orders.push(e.order);
+  });
+
+  let ordersArr = orders.flat();
+  let prices = {
+    "El Hefe": 40,
+    "Fairy Tale Ale": 60,
+    GitHop: 60,
+    "Hollaback Lager": 50,
+    "Hoppily Ever After": 65,
+    Mowintime: 30,
+    "Row 26": 80,
+    "Ruined Childhood": 70,
+    Sleighride: 70,
+    Steampunk: 50,
+  };
+
+  let obj = {};
+  ordersArr.forEach(function (elem) {
+    if (!(elem in obj)) {
+      obj[elem] = { amount: 1, price: prices[elem] };
+    } else {
+      obj[elem].amount += 1;
+    }
+  });
+  /*   var result = [];
+  for (var key in obj) {
+    result.push({ beer: key, amount: obj[key] });
+  } */
+
+  const data = {
+    timestamp: timestamp,
+    queue: queue,
+    accumulated: obj,
+  };
+
+  post(data);
+}
+
+function post(data, endpoint) {
+  const postData = JSON.stringify(data);
+  fetch(endpoint, {
+    method: "post",
+    headers,
+    body: postData,
+  }).then((res) => res.json());
+}
---
+++ settings.js
@@ -0,0 +1,8 @@
+// export of endpoint and headers
+export const endpoint = "https://kea21-4d62.restdb.io/rest/foobar";
+
+export const headers = {
+  "Content-Type": "application/json; charset=utf-8",
+  "x-apikey": "602e36c15ad3610fb5bb62b8",
+  "cache-control": "no-cache",
+};
